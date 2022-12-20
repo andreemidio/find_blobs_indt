5c2c358ee7453f5f66ff7d40b96826d428f861c8
Andre Emidio <andresjc2008@gmail.com>
2022-12-20 14:56:56 -0300

    First Release Test

---
+++ README.md
@@ -0,0 +1,18 @@
+# Desafio de detecção de retanguloes hachurados em uma imagem INDT
+
+### Descrição do Desafio
+
+Desenvolva uma aplicação que dada uma imagem com marcações (ver arquivos de exemplo) localize e informe:
+
+    1. O número de marcações na primeira linha
+
+    2. O número de marcações na primeira coluna
+
+    3. O número de marcações na imagem
+
+Ideia do algoritmo para esse desafio e essas caracteristicas de imagens.
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,92 @@
+from typing import List, Tuple
+
+import cv2
+import numpy as np
+
+
+def read_image(file: str) -> np.array:
+    return cv2.imread(file)
+
+
+def threshold(image: np.array) -> np.array:
+    img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(img_gray, 127, 255, 0)
+
+    return ret, thresh
+
+
+def find_contours(image: np.array) -> np.array:
+    return cv2.findContours(image=image, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_SIMPLE)
+
+
+def inverse(image: np.array) -> np.array:
+    return cv2.bitwise_not(image)
+
+
+def draw_contours(image: np.array, contours, hierarchy):
+    return cv2.drawContours(image, contours, -1, (0, 255, 75), 2)
+
+
+def moments(contours: str) -> np.array:
+    centroids: list = list()
+    for c in contours:
+        M = cv2.moments(c)
+
+        cX = int(M["m10"] / M["m00"])
+        cY = int(M["m01"] / M["m00"])
+
+        centroids.append((cX, cY))
+
+    return centroids
+
+
+def find_min_x_axis(centroids: List[Tuple[int, int]]):
+    min_x_list: list = list()
+
+    for c in centroids:
+        value = 0
+
+        min_x_list.append(c[0])
+
+    min_x_list.sort()
+    min_value = min(min_x_list)
+
+    resultado = list()
+    for m in min_x_list:
+
+        if m == min_value:
+            resultado.append(m)
+
+    return resultado
+
+
+if __name__ == '__main__':
+    file_image: str = "images/4x14x187.bmp"
+    # file_image: str = "images/6x20x198.bmp"
+    r = read_image(file=file_image)
+    ret, image_thresh = threshold(image=r)
+
+    i = inverse(image=image_thresh)
+
+    contours, hierarchy = find_contours(image=i)
+
+    m = moments(contours=contours)
+
+    r_copy = r.copy()
+
+    d = draw_contours(image=r_copy, contours=contours, hierarchy=hierarchy)
+
+    f = find_min_x_axis(centroids=m)
+
+    for row in m:
+        cv2.circle(d, row, 1, (0, 255, 0), 2)
+
+    print(f"Primeira linha há {len(contours[0])} contornos hachurados ")
+
+    print(f"Primeira coluna  há {len(f)} contornos hachurados ")
+
+    print(f"Há um total de  {len(contours)} área hachuradas")
+
+    cv2.imshow("test", d)
+    cv2.imshow("not", i)
+    cv2.waitKey(0)
